8ea29c3844b28b69314d7f158cce5e3907df161a
Debdut Karmakar <iawaiponly@gmail.com>
2020-10-30 19:27:41 +0530

    Footer Prerender

--- prerender.js
+++ prerender.js
@@ -1,10 +1,14 @@
 const Url = require('url-request')
 
 async function render () {
-  const Data = await (Url(`https://api.testflight.live/apps/?_embed=screenshots&_embed=icons`)
+  const AppData = await (Url(`https://api.testflight.live/apps/?_embed=screenshots&_embed=icons`)
     .get())
 
-  const Apps = Data
+  const categoryList = (await (Url('https://api.testflight.live/categories')
+    .get()))
+    .map(c => c.name)
+
+  const Apps = AppData
     .map(d => {
       const app = {}
       app.app = d
@@ -12,22 +16,33 @@ async function render () {
       app.title = `${d.name} - ${d.categories} | Testflight App Store`
       return app
     })
+  
+  for (let i = 0; i < Apps.length; i++) {
+    const app = Apps[i].app
+    const devId = app.devId
+    if (devId) {
+      const dev = await (Url(`https://api.testflight.live/devs/${devId}?_embed=avatars`)
+		.get())
+      app.dev = dev
+      Apps[i].categories = categoryList
+    }
+  }
 
-  const categoryList = (await (Url('https://api.testflight.live/categories')
-    .get()))
   
   let Categories = []
   for (let i = 0; i < categoryList.length; i++) {
     const c = categoryList[i]
     const category = {}
-    category.apps = await (Url(`https://api.testflight.live/apps?_embed=icons&categories_like=${c.name}`)
+    category.apps = await (Url(`https://api.testflight.live/apps?_embed=icons&categories_like=${c}`)
       .get())
-    category.url = `/category/${c.name}`
-    category.title = `Category - ${c.name} | Testflight App Store`
+    category.url = `/category/${c}`
+    category.title = `Category - ${c} | Testflight App Store`
     Categories.push(category)
   }
 
-  return [ ...Categories, ...Apps ]
+  const index =  [{ url: '/', title: 'Testflight App Store | Discover Amazing Apps', categories: categoryList }]
+  
+  return [ ...index, ...Categories, ...Apps ]
 }
 
 module.exports = render
--- src/components/app.js
+++ src/components/app.js
@@ -18,7 +18,7 @@ export default class Application extends Component {
 	constructor(props) {
     super(props)
     this.state = {
-      categories: []
+      categories: props.categories || []
     }
   }
 
